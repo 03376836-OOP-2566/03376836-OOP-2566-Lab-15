aca3bb3e8f2c0692f968ed3031083272628966bd
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-03-26 01:18:17 +0700

    Lab15_Exercise01_result.md

---
+++ Lab15_0258/Lab15_Ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿MyDelegate myDel1 = new MyDelegate(MyMethod);
+MyDelegate myDel2 = MyMethod;
+
+
+myDel1("Hello World");
+myDel1.Invoke("Hello Mars");
+myDel2("Hello Saturn");
+
+static  void  MyMethod(string message)
+{
+    System.Console.WriteLine(message);
+}
+
+public delegate void MyDelegate(string message);
