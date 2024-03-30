f16cd8e3970a4c020d209b6f2221ab615eb62d57
anndyyz <anda2546r@gmail.com>
2024-03-31 06:08:15 +0700

    10

--- 65030276_Lab15/obj/Debug/net8.0/65030276_Lab15.AssemblyInfo.cs
+++ 65030276_Lab15/obj/Debug/net8.0/65030276_Lab15.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("65030276_Lab15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e7072958f8b65acf72a8dad8b814007af0c8ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2d4f826bba74bc90527554d1a7a9de18bdf957")]
 [assembly: System.Reflection.AssemblyProductAttribute("65030276_Lab15")]
 [assembly: System.Reflection.AssemblyTitleAttribute("65030276_Lab15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
